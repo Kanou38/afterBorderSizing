8e0e7affaaa1a0e4f2f7e1da7ab470484f304546
Connected Company <masterconnectedcompany@gmail.com>
2020-02-04 16:38:37 +0100

    improve script including resolution

--- script.js
+++ script.js
@@ -2,75 +2,64 @@ jQuery(document).ready(function($) {
     // Full height mobile menu
     $wh = window.innerrHeight;
     $('.elementor-nav-menu--dropdown').height($wh);
-    setSizeBorderTitle();
+
+    setPropertiesBorderAfterElement();
 });
 
-var resolution = {
+var resolutions = {
     "xs": 460,
     "sm": 768,
     "md": 1170,
-    "lg": 1171
+    "lg": 1171,
 }
 
-function getResolution(s_resolution) {
-    for (var key in resolution) {
-        var value = resolution[key];
-        if (key === s_resolution) {
-            return value;
-        }
-    }
-}
-
-function setSizeBorderTitle() {
-
-    var values = getPropertiesBorderAfterElement();
-    var arrayOfBorderSize = values.size;
-    var arrayOfResolution = values.resolution;
-    var arrayOfBorders = values.borders;
-    var layoutWidth = window.innerWidth;
-
-    // on affecte la valeur à la div parent
-    arrayOfBorders.forEach(function(elem, index = 0) {
-        var resToApplySizing = getResolution(arrayOfResolution[index]);
-
-        if (layoutWidth > "1170" && resToApplySizing > "1170") {
-            elem.style.setProperty("--width", arrayOfBorderSize[index]);
-        }
-        if (layoutWidth <= resToApplySizing && resToApplySizing != "1171") {
-            elem.style.setProperty("--width", arrayOfBorderSize[index]);
-        }
-
-        index++;
-    });
-}
-
-function getPropertiesBorderAfterElement() {
+function setPropertiesBorderAfterElement() {
     /* Get all class of the page who has a gradient title */
     var arrayOfBorders = document.querySelectorAll("[class*=title-gradient-]");
-    var arrayOfBorderSize = [],
-        arrayOfResolution = [];
+    //two dimensino table = key : resolution and for value : border size
+    var arrayOfBordersSettings = [];
 
     /* get all the value of the size and resolution to apply the size */
     for (var i = 0; i < arrayOfBorders.length; i++) {
-        var borderTitle = arrayOfBorders[i];
-        var classLists = borderTitle.classList;
-        console.log(classLists);
+        var borderTitles = arrayOfBorders[i];
+        var classLists = borderTitles.classList;
+
+        var borderSettings = {
+            item: borderTitles,
+            sizes: {}
+        };
+
         for (var j = 0; j < classLists.length; j++) {
+
             if (classLists[j].match(/^title-gradient-/)) {
                 //on récupere la valeur du % de la largeur a affecter
-                var valueGradientSize = borderTitle.classList[j].split('-');
+                var valueGradientSize = borderTitles.classList[j].split('-');
                 var sizeGradientBorder = valueGradientSize[valueGradientSize.length - 1] + "%";
                 var applyOnSpecificResolution = valueGradientSize[valueGradientSize.length - 2];
 
-                arrayOfBorderSize.push(sizeGradientBorder);
-                arrayOfResolution.push(applyOnSpecificResolution);
+                borderSettings.sizes[applyOnSpecificResolution] = sizeGradientBorder;
             }
+        }
+
+        arrayOfBordersSettings.push(borderSettings);
+    }
+
+    //on initialise la resolution initiale
+    var resolutionCurrent = 'lg';
+    var layoutWidth = window.innerWidth;
+
+    for (var resolution in resolutions) {
+        if (layoutWidth <= resolutions[resolution]) {
+            resolutionCurrent = resolution;
+            break;
         }
     }
 
-    return {
-        size: arrayOfBorderSize,
-        resolution: arrayOfResolution,
-        borders: arrayOfBorders
-    };
+    // on affecte la valeur de la taille du pseudo element dans la variable --width
+    arrayOfBordersSettings.forEach(function(elem) {
+        if (!elem.sizes.hasOwnProperty(resolutionCurrent)) {
+            return;
+        }
+        elem.item.style.setProperty("--width", elem.sizes[resolutionCurrent]);
+    });
 }
